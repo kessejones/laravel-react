5cdc1524ae61be00d53262231a2564b9b6d3aaee
Kesse Jones <kjonesfc@outlook.com>
2018-07-19 23:39:59 -0300

    Seed e readme atualizado

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\User::class, 1)->create();
+        \App\User::create([
+            'nome' => 'Usuario 1',
+            'email' => 'user1@test.com',
+            'senha' => bcrypt('senha123'),
+            'remember_token' => str_random(10),
+        ]);
+    }
+}
